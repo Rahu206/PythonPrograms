a15642b1a85bcddc4690c82b4eac750d4732aa55
Rahu206 <rahuldeshpande93@gmail.com>
2021-10-13 09:38:32 +0530

    [Rahul] added:Program to find N-th Harmonic Number

---
+++ harmonicNumber.py
@@ -0,0 +1,32 @@
+'''
+@Author: Rahul Deshpande
+@Date: 2021-10-10 20:10:52
+@Last Modified by: Rahul Deshpande
+@Last Modified time:2021-10-10 20:10:52
+@Title :  Function to find N-th Harmonic Number
+'''
+print("Function to find N-th Harmonic Number")
+
+
+def nthHarmonic(N):  # Harmonic function
+    # H1 = 1
+    harmonic = 1.00
+
+    # loop to apply the formula
+    # Hn = H1 + H2 + H3 ... +
+    # Hn-1 + Hn-1 + 1/n
+    for i in range(2, N + 1):
+        harmonic += 1 / i
+
+    return harmonic
+
+
+# Driver Code
+
+if __name__ == "__main__":
+
+    N = int(input("Enter the Number:"))  #
+    if N != 0:
+        print("The Nth harmonic of '", N, "' is '", round(nthHarmonic(N), 2), "'.")
+    else:
+        print("Enter the Number other than '0'")
